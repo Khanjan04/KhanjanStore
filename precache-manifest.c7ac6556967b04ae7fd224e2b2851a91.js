4768b72a8b16e04ab0ed92ba76fd3bc864dbe93b
Khanjan <khanjanvarma9@gmail.com>
2020-12-23 18:18:23 +0530

    Updates

---
+++ precache-manifest.c7ac6556967b04ae7fd224e2b2851a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44498d21e62708a60e91af9be2584bb6",
+    "url": "/KhanjanStore/index.html"
+  },
+  {
+    "revision": "f2343d65c51285ad5452",
+    "url": "/KhanjanStore/static/css/main.1438c4c1.chunk.css"
+  },
+  {
+    "revision": "4333f10cabedb95c0faa",
+    "url": "/KhanjanStore/static/js/2.da64dcd1.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/KhanjanStore/static/js/2.da64dcd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2343d65c51285ad5452",
+    "url": "/KhanjanStore/static/js/main.f093149b.chunk.js"
+  },
+  {
+    "revision": "85a2e4e2d67c3bd1aece",
+    "url": "/KhanjanStore/static/js/runtime-main.d89fedee.js"
+  }
+]);
